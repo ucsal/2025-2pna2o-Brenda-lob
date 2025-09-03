903bdccf2e9ae18fff60d89a0cf66b64c31a9f82
Brenda <brendaraiana.santos@ucsal.edu.br>
2025-09-02 23:34:22 -0300

    Resolução atividade

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,27 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
+    private final Map<CustomerType, DiscountPolicy> policies;
+    
+    public DiscountCalculator() {
+        this.policies = new HashMap<>();
+        this.policies.put(CustomerType.REGULAR, new RegularPolicy());
+        this.policies.put(CustomerType.PREMIUM, new PremiumPolicy());
+        this.policies.put(CustomerType.PARTNER, new PartnerPolicy());
+    }
+    
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> policies) {
+        this.policies = new HashMap<>(policies);
+    }
+    
+    public double apply(double amount, CustomerType type) {
+        DiscountPolicy policy = policies.get(type);
+        if (policy != null) {
+            return policy.apply(amount);
         }
+        return amount;
     }
 }
